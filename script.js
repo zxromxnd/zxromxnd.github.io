5627d6a8227e2d76848eed4f228db6ae4490e4e0
zxromxnd <zhenya.bryukhovetsky@gmail.com>
2026-03-28 11:56:08 +0200

    Add new files

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-const cartBtn = document.getElementById('cartBtn');
-const cartCount = document.getElementById('cartCount');
+const cartBtn = document.getElementById("cartBtn");
+const cartCount = document.getElementById("cartCount");
 
-cartBtn.addEventListener('click', function(e) {
+cartBtn.addEventListener('click', function(e){
     e.preventDefault();
 
     let count = parseInt(cartCount.textContent);
     cartCount.textContent = count + 1;
-});
+})
